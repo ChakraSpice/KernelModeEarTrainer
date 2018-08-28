0e3315259c5f1a2918e2a315de8a8e64a7464681
Damir Bajramovic <bajramovic.damir@hotmail.com>
2018-08-28 20:41:28 +0200

    Initial commit

---
+++ compile-script.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+export PREFIX="/mnt/c/kernel/opt/cross"
+export TARGET=i686-elf
+export PATH="$PREFIX/bin:$PATH"
+
+rm *.o
+rm os.bin
+rm -r -f isodir
+
+nasm -felf32 start.asm -o start.o
+i686-elf-gcc -c main.c -o main.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
+i686-elf-gcc -c irq.c -o irq.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
+i686-elf-gcc -c gdt.c -o gdt.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
+i686-elf-gcc -c idt.c -o idt.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
+i686-elf-gcc -c isrs.c -o isrs.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
+i686-elf-gcc -c kb.c -o kb.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
+i686-elf-gcc -c timer.c -o timer.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
+i686-elf-gcc -c scrn.c -o scrn.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
+i686-elf-gcc -T linker.ld -o os.bin -ffreestanding -O2 -nostdlib start.o main.o isrs.o irq.o gdt.o idt.o timer.o scrn.o kb.o -lgcc
+grub-file --is-x86-multiboot os.bin
+if grub-file --is-x86-multiboot os.bin; then
+  echo multiboot confirmed
+else
+  echo the file is not multiboot
+fi
+rm -r isodir
+mkdir -p isodir/boot/grub
+cp os.bin isodir/boot/os.bin
+cp grub.cfg isodir/boot/grub/grub.cfg
+grub-mkrescue -o os.iso isodir --xorriso=./xorriso/xorriso-1.4.6/xorriso/xorriso
+
+rm *.o
+rm -r -f isodir
